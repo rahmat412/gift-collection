3cf9422c652a0a1fc238879f60354f96adf71ffc
zafarddhana-m <42727186+rahmat412@users.noreply.github.com>
2023-02-16 12:38:16 +0700

    Update vite.config.js

--- chat-want-to-say/vite.config.js
+++ chat-want-to-say/vite.config.js
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default ({ mode }) => {
   return defineConfig({
+    css: { preprocessorOptions: { css: { charset: false } } },
     base: loadEnv(mode, process.cwd()).VITE_BASE_URL,
     plugins: [vue()]
   })
