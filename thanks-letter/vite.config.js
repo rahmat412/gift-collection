4a5de712e8d01d7189cb8bf4dab8dd17074adc64
zafarddhana-m <42727186+rahmat412@users.noreply.github.com>
2023-02-17 00:17:09 +0700

    update css

--- thanks-letter/vite.config.js
+++ thanks-letter/vite.config.js
@@ -4,6 +4,22 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default ({ mode }) => {
   return defineConfig({
+    css: {
+      postcss: {
+        plugins: [
+          {
+            postcssPlugin: 'internal:charset-removal',
+            AtRule: {
+              charset: (atRule) => {
+                if (atRule.name === 'charset') {
+                  atRule.remove();
+                }
+              }
+            }
+          }
+        ]
+      }
+  },
     base: loadEnv(mode, process.cwd()).VITE_BASE_URL,
     plugins: [vue()]
   })
